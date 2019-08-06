1d8b2439008baa5f2b389180c7e8537afecf186e
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-06 12:54:09 +0530

    add functionallity In CRUD Methods

--- app/app/src/main/java/com/example/groceryappwithgupta/Data/DatabaseHandler.java
+++ app/app/src/main/java/com/example/groceryappwithgupta/Data/DatabaseHandler.java
@@ -1,13 +1,19 @@
 package com.example.groceryappwithgupta.Data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.example.groceryappwithgupta.Model.Grocery;
 import com.example.groceryappwithgupta.Util.Constants;
 
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -40,20 +46,107 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     * */
 
     // Add Grocery
-    public void AddGrocery (Grocery grocery) { }
+    public void AddGrocery (Grocery grocery) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Constants.KEY_GROCERY_ITEM, grocery.getName());
+        values.put(Constants.KEY_QTY_NUMBER, grocery.getQuantity());
+        values.put(Constants.KEY_DATE_NAME, java.lang.System.currentTimeMillis()); // get current system time
+
+        // Insert the row
+        db.insert(Constants.TABLE_NAME, null, values);
+
+        Log.d("Saved", "AddGrocery: Saved to DB");
+
+    }
 
     // Get a Grocery
-    private Grocery getGrocery(int id) { return null; }
+    private Grocery getGrocery(int id) {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Constants.TABLE_NAME, new String[] {
+                Constants.KEY_ID, Constants.KEY_GROCERY_ITEM, Constants.KEY_QTY_NUMBER, Constants.KEY_DATE_NAME
+        }, Constants.KEY_ID + "=?", new String[] {String.valueOf(id)}, null, null, null, null);
+
+        if(cursor != null) {
+            cursor.moveToFirst();
+
+            Grocery grocery = new Grocery();
+            grocery.setId(Integer.parseInt(cursor.getString(cursor.getColumnIndex(Constants.KEY_ID))));
+            grocery.setName(cursor.getString(cursor.getColumnIndex(Constants.KEY_GROCERY_ITEM)));
+            grocery.setQuantity(Integer.parseInt(cursor.getString(cursor.getColumnIndex(Constants.KEY_QTY_NUMBER))));
+
+            // convert timestamp to something readable
+            DateFormat dateFormat = DateFormat.getDateInstance();
+            String formatDate = dateFormat.format(new Date(cursor.getLong(cursor.getColumnIndex(Constants.KEY_DATE_NAME))).getTime());
+            grocery.setDateItemAdded(formatDate);
+        }
+
+        return null;
+    }
 
     // Get all Groceries
-    public List<Grocery> getAllGroceries() { return null; }
+    public List<Grocery> getAllGroceries() {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        List<Grocery> groceriesList = new ArrayList<>();
+
+        Cursor cursor = db.query(Constants.TABLE_NAME, new String[] {
+                Constants.KEY_ID, Constants.KEY_GROCERY_ITEM, Constants.KEY_QTY_NUMBER, Constants.KEY_DATE_NAME
+        }, null, null, null, null, Constants.KEY_DATE_NAME + " DESC");
+
+        if (cursor.moveToFirst()) {
+           do {
+
+               Grocery grocery = new Grocery();
+               grocery.setId(Integer.parseInt(cursor.getString(cursor.getColumnIndex(Constants.KEY_ID))));
+               grocery.setName(cursor.getString(cursor.getColumnIndex(Constants.KEY_GROCERY_ITEM)));
+               grocery.setQuantity(Integer.parseInt(cursor.getString(cursor.getColumnIndex(Constants.KEY_QTY_NUMBER))));
+
+               // convert timestamp to something readable
+               DateFormat dateFormat = DateFormat.getDateInstance();
+               String formatDate = dateFormat.format(new Date(cursor.getLong(cursor.getColumnIndex(Constants.KEY_DATE_NAME))).getTime());
+               grocery.setDateItemAdded(formatDate);
+
+               // add to the groceryList
+               groceriesList.add(grocery);
+
+           } while (cursor.moveToNext());
+        }
+
+        return groceriesList;
+    }
 
     // Update Grocery
-    public int updateGrocery(Grocery grocery) { return 0; }
+    public int updateGrocery(Grocery grocery) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Constants.KEY_GROCERY_ITEM, grocery.getName());
+        values.put(Constants.KEY_QTY_NUMBER, grocery.getQuantity());
+        values.put(Constants.KEY_DATE_NAME, java.lang.System.currentTimeMillis()); // get current system time
+
+        // updated row
+        return db.update(Constants.TABLE_NAME, values,Constants.KEY_ID + "=?", new String[]{String.valueOf(grocery.getId())});
+    }
 
     // Delete Grocery
-    public void deleteGrocery(int id) { }
+    public void deleteGrocery(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        db.delete(Constants.TABLE_NAME, Constants.KEY_ID + "=?", new String[] { String.valueOf(id)});
+
+        db.close();
+    }
 
     // Get Count
-    public int getGroceriesCount() { return 0; }
+    public int getGroceriesCount() {
+        String countQuery = "SELECT * FROM " + Constants.TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(countQuery, null);
+
+        return cursor.getCount();
+    }
 }
