cb73090ae93fd709b54498c13adc4ced2595d3a1
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-07 13:46:50 +0530

    Add Items While In List View Page

--- app/app/src/main/java/com/example/groceryappwithgupta/Activities/ListActivity.java
+++ app/app/src/main/java/com/example/groceryappwithgupta/Activities/ListActivity.java
@@ -1,13 +1,19 @@
 package com.example.groceryappwithgupta.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.groceryappwithgupta.Data.DatabaseHandler;
 import com.example.groceryappwithgupta.Model.Grocery;
@@ -20,10 +26,17 @@ import java.util.List;
 public class ListActivity extends AppCompatActivity {
 
     private DatabaseHandler db;
-    private RecyclerView recyclerView;
-    private List<Grocery> groceryList;
-    private List<Grocery> listItems;
-    private RecyclerViewAdaptor recyclerViewAdaptor;
+    RecyclerView recyclerView;
+    List<Grocery> groceryList;
+    List<Grocery> listItems;
+    RecyclerViewAdaptor recyclerViewAdaptor;
+
+    AlertDialog.Builder dialogBuilder;
+    AlertDialog dialog;
+
+    EditText groceryItem;
+    EditText quantity;
+    Button saveBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +49,7 @@ public class ListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
+                createPopUpDialog();
             }
         });
 
@@ -66,6 +78,57 @@ public class ListActivity extends AppCompatActivity {
         recyclerView.setAdapter(recyclerViewAdaptor);
         recyclerViewAdaptor.notifyDataSetChanged(); // something has change. which will handle by recycleView we don't have to do anything
 
+    }
+
+    private void createPopUpDialog() {
+        dialogBuilder = new AlertDialog.Builder(this);
+
+        View view = getLayoutInflater().inflate(R.layout.popup, null);
+
+        groceryItem = view.findViewById(R.id.popup_grocery_item_edit_text);
+        quantity = view.findViewById(R.id.popup_grocery_qty_edit_text);
+        saveBtn = view.findViewById(R.id.popup_save_btn);
+
+        dialogBuilder.setView(view);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        // actions to be perform after clicking save button
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            // Save to db
+            if (!TextUtils.isEmpty(groceryItem.getText()) && !TextUtils.isEmpty(quantity.getText())) {
+                saveGroceryToDB(view);
+            }
+            }
+        });
+    }
+
+    private void saveGroceryToDB(View v) {
+
+        // create an object
+        Grocery grocery = new Grocery();
+
+        String newGrocery = groceryItem.getText().toString();
+        int newGroceryQuantity = Integer.parseInt(quantity.getText().toString());
+
+        grocery.setName(newGrocery);
+        grocery.setQuantity(newGroceryQuantity);
+
+        // Save to db
+        db.addGrocery(grocery);
+        Snackbar.make(v, "Item Saved!", Snackbar.LENGTH_LONG).show();
+        Log.d("Item Added ID: ", String.valueOf(db.getGroceriesCount()));
+
+        // reassigning listItems To recycleView
+        listItems.add(grocery);
+        recyclerViewAdaptor = new RecyclerViewAdaptor(this, listItems);
+        recyclerView.setAdapter(recyclerViewAdaptor);
+        recyclerViewAdaptor.notifyDataSetChanged();
+
+        dialog.dismiss();
     }
 
 }
